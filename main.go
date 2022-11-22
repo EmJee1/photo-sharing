36676576039b461adaccf294f8a03632c3b8058e
EmJee <mart-janroeleveld@outlook.com>
2022-11-22 11:22:09 +0100

    Add gorm configuration with postgres

--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"net/http"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"photo-sharing/handlers"
+	"time"
 )
+
+type User struct {
+	ID        uint
+	UserName  string
+	CreatedAt time.Time
+}
 
 func main() {
 	e := echo.New()
+
+	dsn := "host=localhost user=postgres password=password dbname=postgres port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Could not connect to database")
+	}
+
+	db.AutoMigrate(&User{})
+	db.Create(&User{UserName: "Mart-Jan"})
 
 	// TODO: add is-logged-in middleware
 	e.GET("/", func(c echo.Context) error {
