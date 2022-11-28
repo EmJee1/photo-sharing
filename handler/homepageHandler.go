9a70d10a8346a993e24af3877c7fa24af3fc48bb
EmJee <mart-janroeleveld@outlook.com>
2022-11-28 10:28:06 +0100

    partials update & remove lit

--- handler/homepageHandler.go
+++ handler/homepageHandler.go
@@ -4,10 +4,12 @@ import (
 	"github.com/foolin/goview/supports/echoview-v4"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"photo-sharing/model"
 )
 
 func GetHomepage(context echo.Context) error {
 	return echoview.Render(context, http.StatusOK, "homepage", echo.Map{
 		"title": "Overview",
+		"user":  model.User{Email: "john@doe.com"},
 	})
 }
