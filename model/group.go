e35f54c76eeb739b9b32af8cece44eafe5d88ff9
EmJee <mart-janroeleveld@outlook.com>
2022-12-01 08:06:54 +0100

    Add container, grid and optional description on group

--- model/group.go
+++ model/group.go
@@ -1,7 +1,8 @@
 package model
 
 type Group struct {
-	ID    uint
-	Name  string
-	Users []*User `gorm:"many2many:group_users;"`
+	ID          uint
+	Name        string
+	Description string
+	Users       []*User `gorm:"many2many:group_users;"`
 }
