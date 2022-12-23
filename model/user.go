385a88b4a6caa35b218accc4bafed35eb126c26d
EmJee <mart-janroeleveld@outlook.com>
2022-12-23 16:57:39 +0100

    Add username to user model

--- model/user.go
+++ model/user.go
@@ -6,6 +6,7 @@ type User struct {
 	ID           uint
 	Email        string
 	Password     string
+	Username     string
 	CreatedAt    time.Time
 	Posts        []Post
 	Groups       []*Group      `gorm:"many2many:group_users;"`
