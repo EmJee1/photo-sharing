fe362aee4d0f683bee74ca3daac3fd181a8aa069
EmJee <mart-janroeleveld@outlook.com>
2023-01-05 15:00:41 +0100

    Add feed

--- handler/homepageHandler.go
+++ handler/homepageHandler.go
@@ -3,21 +3,34 @@ package handler
 import (
 	"github.com/foolin/goview/supports/echoview-v4"
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm/clause"
 	"net/http"
 	"photo-sharing/model"
 	"photo-sharing/repository"
 )
 
 func GetHomepage(context echo.Context) error {
+	userId := context.Get("userId").(uint)
+
 	user := &model.User{}
 	repository.GetUser(
-		context.Get("userId").(uint),
+		userId,
 		&user,
-		"Groups.Posts.Likes",
+		"Groups.Posts.Comments.User",
+		"Groups.Posts."+clause.Associations,
 	)
 
+	var feed []model.Post
+	for _, group := range user.Groups {
+		for _, post := range group.Posts {
+			feed = append(feed, post)
+		}
+	}
+
 	return echoview.Render(context, http.StatusOK, "homepage", echo.Map{
-		"title": "Overview",
-		"user":  user,
+		"title":  "Overview",
+		"user":   user,
+		"feed":   feed,
+		"userId": userId,
 	})
 }
